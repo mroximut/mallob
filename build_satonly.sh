c19f0ab2eda26792607e38214f415476041f4b17
mroximut <oguzmutlu2001@gmail.com>
2025-07-06 01:12:54 +0200

    build scripts corrected at server

--- build_satonly.sh
+++ build_satonly.sh
@@ -3,11 +3,11 @@ mkdir -p build
 cd build
 
 
-if $SERVER == "1" ; then
+if [ $SERVER == "1" ] ; then
 echo "Building for server..."
 CC=$(which mpicc) CXX=$(which mpicxx) cmake -DCMAKE_BUILD_TYPE=RELEASE -DMALLOB_APP_SAT=1 \
--DMALLOB_JEMALLOC_DIR=/nfs/home/omutlu/.user_spack/environments/myenv/.spack-env/._view/lib \
--DMALLOB_LOG_VERBOSITY=4 -DMALLOB_ASSERT=1 -DMALLOB_SUBPROC_DISPATCH_PATH=\"build_2ls/\" ..
+-DMALLOB_JEMALLOC_DIR=/nfs/home/omutlu/.user_spack/environments/myenv/.spack-env/view/lib -DMALLOB_USE_JEMALLOC=1 \
+-DMALLOB_LOG_VERBOSITY=4 -DMALLOB_ASSERT=1 -DMALLOB_SUBPROC_DISPATCH_PATH=\"build/\" ..
 make -j32
 cd ..
 else 
--- build_with_2ls.sh
+++ build_with_2ls.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [ ! -d "lib/2ls" ]; then
-  mkdir -p lib
   cd lib
   git clone git@github.com:mroximut/2ls.git --branch app_for_mallob
   cd ..
@@ -13,10 +12,10 @@ mkdir -p build_2ls
 rm ./build_2ls/*mallob*
 cd build_2ls
 
-if $SERVER == "1" ; then
+if [ $SERVER == "1" ] ; then
 echo "Building for server..."
 CC=$(which mpicc) CXX=$(which mpicxx) cmake -DCMAKE_BUILD_TYPE=RELEASE -DMALLOB_APP_SAT=1 -DMALLOB_APP_2LS=1 \
--DMALLOB_JEMALLOC_DIR=/nfs/home/omutlu/.user_spack/environments/myenv/.spack-env/._view/lib \
+-DMALLOB_JEMALLOC_DIR=/nfs/home/omutlu/.user_spack/environments/myenv/.spack-env/view/lib -DMALLOB_USE_JEMALLOC=1 \
 -DMALLOB_LOG_VERBOSITY=4 -DMALLOB_ASSERT=1 -DMALLOB_SUBPROC_DISPATCH_PATH=\"build_2ls/\" ..
 make -j32
 cd ..
